4423c4bc29de4d484f2fc002f72e8367c1403814
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-10-12 18:02:39 +0545

    feat: Add Todo

--- src/components/todos/AddTodo.jsx
+++ src/components/todos/AddTodo.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import {addTodo} from "../../features/todos/todoSlice";
+
 
 function AddTodo() {
+    const [input, setInput] = useState();
+    const dispatch = useDispatch();
+
+    const handleForm = (e) => {
+        e.preventDefault();
+        dispatch(addTodo({text:input}));
+        setInput('');
+    }
     return (
-        <form className="space-x-3 mt-12">
+        <form onSubmit={handleForm} className="space-x-3 mt-12">
             <input
                 type="text"
                 className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 placeholder="Enter a Todo..."
+                value={input}
+                onChange={(e)=>setInput(e.target.value)}
             />
             <button
                 type="submit"
