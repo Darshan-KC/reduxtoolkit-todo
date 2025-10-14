f855dc184f4f59411e3bd39ff6d61f36a0797fad
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-10-14 21:33:13 +0545

    feat: dispact in todo

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -15,6 +15,7 @@ function Todos() {
                             className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded">
                             <div className='text-white'>{todo.text}</div>
                             <button className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+                            onClick={() => dispatch(removeTodo({id: todo.id}))}
                             >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
