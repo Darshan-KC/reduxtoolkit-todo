4df3b735f755f3cf91e97a9289daba27600de287
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-10-16 20:59:52 +0545

    fix: error

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -7,15 +7,16 @@ function Todos() {
     const todos = useSelector(state => state.todos);
     return (
         <>
-            <div>Todos</div>
+            <div className="text-center text-2xl my-2">Todos</div>
             <ul className="list-none">
                 {
                     todos.map((todo) => (
                         <li
+                            key={todo.id}
                             className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded">
                             <div className='text-white'>{todo.text}</div>
                             <button className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
-                            onClick={() => dispatch(removeTodo({id: todo.id}))}
+                                onClick={() => dispatch(removeTodo({ id: todo.id }))}
                             >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
